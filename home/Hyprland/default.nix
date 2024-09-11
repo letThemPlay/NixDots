9100b3f21320bc2a46cd4ee167b352d4eb31c8fd
rachitve6h2g <rachitverma1122@gmail.com>
2024-09-11 14:15:00 +0530

    New Start, Better Start

--- home/Hyprland/default.nix
+++
@@ -1,9 +0,0 @@
-{
-	imports = [
-		./hyprland.nix
-		./waybar.nix
-		./wofi.nix
-    ./miscServices.nix
-    ./wlogout.nix
-	];
-}
--- home/Programs/Shells/default.nix
+++
@@ -1,6 +0,0 @@
-{
-	imports = [
-		./kitty.nix
-    ./shells.nix
-	];
-}
